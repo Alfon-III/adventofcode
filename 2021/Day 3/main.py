5b5ddcf14f197ca90b0673f6fb1f57c985ae0666
Alfon <altercer@ucm.es>
2021-12-05 12:28:05 +0100

    Comentarios para Arturo

--- 2021/Day 3/main.py
+++ 2021/Day 3/main.py
@@ -8,9 +8,12 @@ with open("Day 3/data.txt") as file_in:
 aux = ""
 
 for n in range(12):
+    #Array de caracteres del n elemento de cada fila (leer en vertical)
     letters_list = [x[n] for x in lines if len(x)>0]
+    #Se obtiene el caracter (0 o 1) más repetido de la lista
     aux = aux + statistics.mode(letters_list)
-    print(aux)
+    
+print(aux)
 
 #Aux devuelve el código en decimal.
 #Traducir con la calculadora de programador de windows
